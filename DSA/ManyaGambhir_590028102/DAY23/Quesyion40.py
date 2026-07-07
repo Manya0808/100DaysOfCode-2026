ef4f5fafd8c191c4e4c934377db7559c3bc206c9
Manya0808 <Manya.28102@stu.upes.ac.in>
2026-07-07 21:09:51 +0530

    day23 questions

---
+++ DSA/ManyaGambhir_590028102/DAY23/Quesyion40.py
@@ -0,0 +1,12 @@
+def backspaceCompare(s, t):
+    def process(string):
+        stack = []
+        for ch in string:
+            if ch == '#':
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(ch)
+        return "".join(stack)
+
+    return process(s) == process(t)
